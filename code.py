1b827a16e1d4b216feb92ffd5b8b872be011cb2d
Tanisha-021158 <tanishasaha225@gmail.com>
2024-12-28 19:21:58 +0530

    modified code

--- code.py
+++ code.py
@@ -1,11 +1,26 @@
 import random
 name=input("Enter your name:")
-print("Welcome ",name," !")
+print("Welcome",name,"!")
+lives=3
 score=0
-while True:
+while lives>0:
     print("Do you want to roll your die?")
     ans=input("y for yes/ n for no:").lower()
     if ans=='y':
         print("Rolling your die...")
         op=random.randint(1,6)
-        print(op)
+        print("You got:",op)
+        if op==1:
+            print("You lose! Your score becomes zero")
+            score=0
+            lives-=1
+            print("You have",lives,"lives left!")
+        else:
+            score=score+op
+            print("Your score:",score)
+    elif ans=='n':
+        print("Your score:",score)
+        break
+    else:
+        print("Wrong choice")
+print("Thank you!")
